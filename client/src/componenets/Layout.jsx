461662f71af69e51d688b764836a3a5ed092fb2f
VivekDev01 <vivek65.ugcs20@iiitranchi.ac.in>
2023-06-26 02:39:43 +0530

    some changes in layout

--- client/src/componenets/Layout.jsx
+++ client/src/componenets/Layout.jsx
@@ -57,12 +57,141 @@ const Layout = ({ children }) => {
   return (
     <>
       <div className="main">
-        <div className="layout">
+        {/* ====================================================================================================================================== */}
+
+        <header className="header">
+          <nav class="style-4">
+            <ul class="menu-4">
+              <li class="current">
+                <a href="#" data-hover="Home">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" data-hover="About Us">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#" data-hover="Blog">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a href="#" data-hover="Services">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href="#" data-hover="Products">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="#" data-hover="Contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <div className="first justify-content-center"></div>
+
+        {/* ============================================================================================================================================ */}
+
+        <div>
+          <input type="checkbox" id="check" />
+          <label className="button bars" htmlFor="check">
+            <i className="fas fa-bars" />
+          </label>
+          <div className="side_bar">
+            <div className="title">
+              <div className="logo">WellBeing</div>
+              <label className=" button cancel" htmlFor="check">
+                <i className="fas fa-times" />
+              </label>
+            </div>
+            <ul>
+              <li>
+                <a href="#">
+                  <i className="fas fa-qrcode" />
+                  Dashboard
+                </a>
+              </li>
+
+              {/* --------------------------------------------- */}
+              {sidebarMenu &&
+                sidebarMenu.map((menu) => {
+                  const isActive = location.pathname === menu.path;
+                  return (
+                    <div
+                      className={`menu-item ${isActive && "active"}`}
+                      key={menu.path}
+                    >
+                      <li>
+                        <a href={menu.path}>
+                          <i className={menu.icon} />
+                          {menu.name}
+                        </a>
+                      </li>
+                    </div>
+                  );
+                })}
+              <div className="menu-item" onClick={handleLogout}>
+                <li>
+                  <a href="/login">
+                    <i className="ri-logout-box-r-line" />
+                    Logout
+                  </a>
+                </li>
+              </div>
+
+              {/* --------------------------------------------- */}
+            </ul>
+            <div className="media_icons">
+              <a href="#">
+                <i className="fab fa-facebook-f" />
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter" />
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram" />
+              </a>
+              <a href="#">
+                <i className="fab fa-youtube" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* ================================================================================================================================================================ */}
+
+        {/* <div className="layout">
+        <div className="header">
+              <div className="header-content" style={{ cursor: "pointer" }}>
+                <Badge
+                  count={user ? user.Notification.length : 0}
+                  onClick={() => {
+                    Navigate("/notification");
+                  }}
+                >
+                  <i className="ri-notification-3-line"></i>
+                </Badge>
+
+                <Link to="/profile">{user ? user.name : ""}</Link>
+              </div>
+        </div>
+          
+          <div className="content">
+            
           <div className="sidebar">
             <div className="logo">
               <h6>WellBeing</h6>
               <hr />
             </div>
+
             <div className="menu">
               {sidebarMenu &&
                 sidebarMenu.map((menu) => {
@@ -83,24 +212,10 @@ const Layout = ({ children }) => {
               </div>
             </div>
           </div>
-          <div className="content">
-            <div className="header">
-              <div className="header-content" style={{ cursor: "pointer" }}>
-                <Badge
-                  count={user ? user.Notification.length : 0}
-                  onClick={() => {
-                    Navigate("/notification");
-                  }}
-                >
-                  <i className="ri-notification-3-line"></i>
-                </Badge>
 
-                <Link to="/profile">{user ? user.name : ""}</Link>
-              </div>
-            </div>
             <div className="body">{children}</div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );

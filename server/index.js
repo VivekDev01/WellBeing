96019e331d601a1e0a8c5aa4ec651ce320b3c65e
VivekDevShah <vivek65.ugcs20@iiitranchi.ac.in>
2023-08-03 18:25:23 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -29,6 +29,18 @@ app.use("/api/v1/hospital", hospitalRouter);
 
 //listen port
 const port=process.env.PORT || 4000
+
+// deployment config
+import path from "path"
+let __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, ()=>{
     console.log(`Server Running in ${process.env.NODE_MODE} Mode on port ${port}`.bgCyan.white);
 });
